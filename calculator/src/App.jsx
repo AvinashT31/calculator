a9a44e75969190adef561c7c2b675f796b4c6c4c
AvinashT31 <59430438+AvinashT31@users.noreply.github.com>
2023-03-11 23:34:13 +0530

    update

---
+++ calculator/src/App.jsx
@@ -0,0 +1,39 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div id='calculator'>
+        <input type="text" />
+        <div>
+          <button>7</button>
+          <button>8</button>
+          <button>9</button>
+          <button>/</button>
+        </div>
+        <div>
+          <button>4</button>
+          <button>5</button>
+          <button>6</button>
+          <button>*</button>
+        </div>
+        <div>
+          <button>1</button>
+          <button>2</button>
+          <button>3</button>
+          <button>-</button>
+        </div>
+        <div>
+          <button>0</button>
+          <button>.</button>
+          <button>+</button>
+          <button>=</button>
+        </div>
+        
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
